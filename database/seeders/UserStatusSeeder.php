e9fb80010196b2c92c80b5cc1f691e1e1d6f22ae
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-22 08:55:59 +0900

    feat: m_user_status に初期データを追加する処理の実装

--- database/seeders/UserStatusSeeder.php
+++ database/seeders/UserStatusSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserStatusSeeder extends Seeder
 {
@@ -12,6 +13,27 @@ class UserStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('m_user_status')->insert([
+            [
+                'user_status' => '自席',
+                'created_at' => now(),
+            ],
+            [
+                'user_status' => '離席',
+                'created_at' => now(),
+            ],
+            [
+                'user_status' => '会議中',
+                'created_at' => now(),
+            ],
+            [
+                'user_status' => '外出中',
+                'created_at' => now(),
+            ],
+            [
+                'user_status' => 'なし',
+                'created_at' => now(),
+            ]
+        ]);
     }
 }
