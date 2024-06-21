a72678dc60c7e03709352ceeba17d0ead6c7cdf2
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-22 08:56:07 +0900

    feat: m_user に初期データを追加する処理の実装

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('m_user')->insert([
+            [
+                'mail_address' => 'kikuuchi@itcowork.co.jp',
+                'password' => Hash::make('password'),
+                'user_name' => '菊内尊雄',
+                'user_name_kana' => 'きくうちたかお',
+                'default_area_id' => 1,
+                'is_admin' => true,
+                'attendance_id' => 6,
+                'user_status_id' => 5,
+                'created_at' => now(),
+            ],
+        ]);
     }
 }
