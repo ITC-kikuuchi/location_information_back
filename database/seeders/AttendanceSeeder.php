ba7f051eb374888b67240ba92ce8c6cddc2903ae
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-22 08:55:51 +0900

    feat: m_attendance に初期データを追加する処理の実装

--- database/seeders/AttendanceSeeder.php
+++ database/seeders/AttendanceSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AttendanceSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('m_attendance')->insert([
+            [
+                'attendance_status' => '出社',
+                'created_at' => now(),
+            ],
+            [
+                'attendance_status' => 'リモート',
+                'created_at' => now(),
+            ],
+            [
+                'attendance_status' => '退勤',
+                'created_at' => now(),
+            ],
+            [
+                'attendance_status' => '出張',
+                'created_at' => now(),
+            ],
+            [
+                'attendance_status' => '休暇',
+                'created_at' => now(),
+            ],
+            [
+                'attendance_status' => 'なし',
+                'created_at' => now(),
+            ]
+        ]);
     }
 }
