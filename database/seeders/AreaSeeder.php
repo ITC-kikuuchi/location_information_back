0cb67e9bd21859b67f930e58928e07479b3cf0b4
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-22 08:55:34 +0900

    feat: m_area に初期データを追加する処理の実装

--- database/seeders/AreaSeeder.php
+++ database/seeders/AreaSeeder.php
@@ -13,6 +13,37 @@ class AreaSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('m_area')-
+        DB::table('m_area')->insert([
+            [
+                'area_name' => '2F 開発部屋',
+                'is_default_area' => true,
+                'created_at' => now(),
+            ],
+            [
+                'area_name' => '2F 会議室',
+                'is_default_area' => false,
+                'created_at' => now(),
+            ],
+            [
+                'area_name' => '1F 開発部屋',
+                'is_default_area' => true,
+                'created_at' => now(),
+            ],
+            [
+                'area_name' => '1F 会議室',
+                'is_default_area' => false,
+                'created_at' => now(),
+            ],
+            [
+                'area_name' => '自宅',
+                'is_default_area' => true,
+                'created_at' => now(),
+            ],
+            [
+                'area_name' => 'なし',
+                'is_default_area' => false,
+                'created_at' => now(),
+            ]
+        ]);
     }
 }
