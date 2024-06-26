994185db773dd24ff6b3dbf3c62901ca21b844a3
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 15:06:08 +0900

    feat: バインディング処理の実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(
+            \App\Repositories\User\UserRepositoryInterface::class,
+            \App\Repositories\User\UserEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\Area\AreaRepositoryInterface::class,
+            \App\Repositories\Area\AreaEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\Attendance\AttendanceRepositoryInterface::class,
+            \App\Repositories\Attendance\AttendanceEloquentRepository::class
+        );
+        $this->app->bind(
+            \App\Repositories\UserStatus\UserStatusRepositoryInterface::class,
+            \App\Repositories\UserStatus\UserStatusEloquentRepository::class
+        );
     }
 }
