c26b3617bfdaf1b27cbda35b6bbdf8741b94cea3
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 14:54:43 +0900

    feat: route の実装

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// ログインAPI
+Route::post('login', [AuthController::class, 'login']);
+// ログアウトAPI
+Route::post('logout', [AuthController::class, 'logout']);
+
+// ミドルウェア auth:sanctum を指定したグループ
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // ログイン情報取得API
+    Route::get('me', [AuthController::class, 'me']);
+    // 社員情報API
+    Route::apiResource('Users', UserController::class, ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+    // エリアAPI
+    Route::apiResource('Areas', AreaController::class, ['only' => ['index', 'store', 'show', 'update', 'destroy']]);
+    // 勤怠API
+    Route::get('attendance', AttendanceController::class);
+    // ユーザステータスAPI
+    Route::get('user-status', UserStatusController::class);
+    // 位置情報API
+    Route::get('user-location', UserLocationController::class, ['only' => ['index', 'show', 'update']]);
+});
