a0b1f6cd3f0b114f10cd8c0fe24dda31e3a72392
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 14:02:05 +0900

    feat: AttendanceController クラスを定義したファイルの実装

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function __invoke()
+    {
+        //
+    }
+}
