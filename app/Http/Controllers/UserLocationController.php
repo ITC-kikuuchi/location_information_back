4315f0085896b888fff723a69d3a83ffffb32e4e
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 14:02:33 +0900

    feat: UserLocationController クラスを定義したファイルの実装

---
+++ app/Http/Controllers/UserLocationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+}
