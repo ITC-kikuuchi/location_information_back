797157b3f4b243fca6b47106d07cf1e81b806cbc
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 14:02:56 +0900

    feat: AuthController クラスを定義したファイルの実装

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * ログインAPI
+     */
+    public function login()
+    {
+        //
+    }
+
+    /**
+     * ログイン情報取得API
+     */
+    public function me()
+    {
+        //
+    }
+
+    /**
+     * ログアウトAPI
+     */
+    public function logout()
+    {
+        //
+    }
+}
