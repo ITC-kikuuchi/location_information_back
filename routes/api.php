86894a7565dfda44ed4b1133b632b1a6f6b2e400
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-26 14:54:36 +0900

    feat: use 処理の実装

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserLocationController;
+use App\Http\Controllers\UserStatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
