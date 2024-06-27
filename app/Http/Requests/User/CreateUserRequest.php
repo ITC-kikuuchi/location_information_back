468047ca653f76e493c181273095318510f6d755
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-27 16:59:26 +0900

    feat: use 処理の実装

--- app/Http/Requests/User/CreateUserRequest.php
+++ app/Http/Requests/User/CreateUserRequest.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Requests\User;
 
+use App\Models\User;
+use App\Traits\ResponseTrait;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class CreateUserRequest extends FormRequest
 {
+    use ResponseTrait;
+
     /**
      * Determine if the user is authorized to make this request.
      */
