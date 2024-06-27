9cb3fd131e9af94d49b56a91246f6818fc2539ad
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-27 10:06:11 +0900

    feat: ユーザ一覧取得処理の実装

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -25,4 +25,33 @@ class UserService
     public function __construct(protected UserRepositoryInterface $userRepositoryInterface)
     {
     }
+
+    /**
+     * ユーザ一覧取得
+     *
+     * @return JsonResponse
+     */
+    public function getUsers(): JsonResponse
+    {
+        $responseData = [];
+        try {
+            // ユーザ一覧取得
+            $users = $this->userRepositoryInterface->getUsers();
+            // レスポンスデータの作成
+            foreach($users as $user) {
+                $responseData[User::USER_LIST][] = [
+                    User::ID => $user[User::ID],
+                    User::USER_NAME => $user[User::USER_NAME],
+                    User::USER_NAME_KANA => $user[User::USER_NAME_KANA],
+                    User::MAIL_ADDRESS => $user[User::MAIL_ADDRESS],
+                    User::IS_ADMIN => (bool)$user[User::IS_ADMIN],
+                ];
+            }
+        } catch (Exception $e) {
+            // エラーハンドリング
+            return $this->exceptionHandler($e);
+        }
+        // 200 レスポンス
+        return $this->okResponse($responseData);
+    }
 }
