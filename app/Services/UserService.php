5efee0bae3cfb7cd104d4b37c726ddf9096e5ab6
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-27 10:04:00 +0900

    feat: use 処理の実装

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\User;
+use App\Repositories\User\UserRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class UserService
 {
+    use ResponseTrait;
+    use ExceptionHandlerTrait;
 }
