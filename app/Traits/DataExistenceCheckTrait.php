95e21cfd8587d87588dcb3bdbd3326f667abd26b
kikuuchi <kikuuchi@itcowork.co.jp>
2024-06-28 11:14:27 +0900

    feat: データ存在チェック処理の実装

--- app/Traits/DataExistenceCheckTrait.php
+++ app/Traits/DataExistenceCheckTrait.php
@@ -8,4 +8,16 @@ use App\Exceptions\NotFoundException;
 
 trait DataExistenceCheckTrait
 {
+    /**
+     * データ存在チェック処理
+     *
+     * @param object|null $data
+     */
+    public function dataExistenceCheck(object|null $data): void
+    {
+        if (!$data) {
+            // データが存在しなかった場合
+            throw new NotFoundException();
+        }
+    }
 }
