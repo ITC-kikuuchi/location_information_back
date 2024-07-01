b486655ddf66929811a20050ec750868c51da5be
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-01 11:10:37 +0900

    feat: コンストラクタメソッドの実装

--- app/Services/AreaService.php
+++ app/Services/AreaService.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Repositories\Area\AreaRepositoryInterface;
+
 class AreaService
 {
+    /**
+     * AreaService コンストラクタ
+     * AreaRepositoryInterface の依存性を注入する
+     *
+     * @param AreaRepositoryInterface $areaRepositoryInterface
+     */
+    public function __construct(protected AreaRepositoryInterface $areaRepositoryInterface)
+    {
+    }
 }
