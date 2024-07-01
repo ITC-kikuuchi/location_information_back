8b174e2fe47ac2b42bdaeb48715bab6ffd1393d2
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-01 13:12:34 +0900

    feat: use 処理の実装

--- app/Services/AreaService.php
+++ app/Services/AreaService.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Area;
 use App\Repositories\Area\AreaRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
 
 class AreaService
 {
+    use ResponseTrait;
+    use ExceptionHandlerTrait;
+
     /**
      * AreaService コンストラクタ
      * AreaRepositoryInterface の依存性を注入する
