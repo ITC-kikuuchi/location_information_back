dec76ea5b6928651a4e72805d5744b12cb9dc6c4
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-01 13:12:19 +0900

    feat: エリア一覧取得処理の実装

--- app/Repositories/Area/AreaEloquentRepository.php
+++ app/Repositories/Area/AreaEloquentRepository.php
@@ -17,4 +17,14 @@ class AreaEloquentRepository implements AreaRepositoryInterface
     public function __construct(protected Area $area)
     {
     }
+
+    /**
+     * エリア一覧取得
+     *
+     * @return object|null
+     */
+    public function getAreas(): object|null
+    {
+        return $this->area->get();
+    }
 }
