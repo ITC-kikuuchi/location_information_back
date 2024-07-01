526461c3796fb856e6482d7337ba9af1f37ea5a8
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-01 13:12:01 +0900

    feat: コンストラクタメソッドの実装

--- app/Repositories/Area/AreaEloquentRepository.php
+++ app/Repositories/Area/AreaEloquentRepository.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Repositories\Area;
 
+use App\Models\Area;
+
 class AreaEloquentRepository implements AreaRepositoryInterface
 {
+    /**
+     * AreaEloquentRepository コンストラクタ
+     * Area の依存性を注入する
+     *
+     * @param Area $area
+     */
+    public function __construct(protected Area $area)
+    {
+    }
 }
