52914bcbfb814db97df48fd4ca9d4287fd86684f
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-03 14:28:51 +0900

    feat: use 処理の実装

--- app/Traits/ExecutionAuthorityCheckTrait.php
+++ app/Traits/ExecutionAuthorityCheckTrait.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Traits;
 
+use App\Exceptions\ForbiddenException;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 trait ExecutionAuthorityCheckTrait
 {
 }
