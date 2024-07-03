4dc53a96aafdab6bdc031d89616b754d9ce7dca2
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-03 14:29:06 +0900

    feat: 実行権限チェック処理の実装

--- app/Traits/ExecutionAuthorityCheckTrait.php
+++ app/Traits/ExecutionAuthorityCheckTrait.php
@@ -10,4 +10,22 @@ use Illuminate\Support\Facades\Auth;
 
 trait ExecutionAuthorityCheckTrait
 {
+    /**
+     * 実行権限チェック
+     *
+     * @param int|null $userId
+     * @return void
+     */
+    public function ExecutionAuthorityCheck(int|null $userId = null)
+    {
+        // 認証済みユーザの取得
+        $user = Auth::user();
+        if (!$user[User::IS_ADMIN]) {
+            // 管理者権限が存在しない場合
+            if (!$userId || $user[User::ID] != $userId) {
+                // ユーザID が存在しない、またはログインユーザのID と一致しない場合
+                throw new ForbiddenException();
+            }
+        }
+    }
 }
