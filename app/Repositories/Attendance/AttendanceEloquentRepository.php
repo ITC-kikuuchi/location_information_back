89696c76e3f97f981c64c492fea80498944f93ad
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-05 16:29:18 +0900

    feat: コンストラクタメソッドの実装

--- app/Repositories/Attendance/AttendanceEloquentRepository.php
+++ app/Repositories/Attendance/AttendanceEloquentRepository.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Repositories\Attendance;
 
+use App\Models\Attendance;
+
 class AttendanceEloquentRepository implements AttendanceRepositoryInterface
 {
+    /**
+     * AttendanceEloquentRepository コンストラクタ
+     * Attendance の依存性を注入する
+     *
+     * @param Attendance $attendance
+     */
+    public function __construct(protected Attendance $attendance)
+    {
+    }
 }
