5b3f68d79eff1aaf66e04fef2590c652244339b4
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-05 17:03:02 +0900

    feat: コンストラクタメソッドの実装

--- app/Repositories/UserStatus/UserStatusEloquentRepository.php
+++ app/Repositories/UserStatus/UserStatusEloquentRepository.php
@@ -4,6 +4,17 @@ declare(strict_types=1);
 
 namespace App\Repositories\UserStatus;
 
+use App\Models\UserStatus;
+
 class UserStatusEloquentRepository implements UserStatusRepositoryInterface
 {
+    /**
+     * UserStatusEloquentRepository コンストラクタ
+     * UserStatus の依存性を注入する
+     *
+     * @param UserStatus $userStatus
+     */
+    public function __construct(protected UserStatus $userStatus)
+    {
+    }
 }
