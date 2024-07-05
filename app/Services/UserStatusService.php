2e0d61d38272cfeaf12506e03cc1d23361f81112
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-05 17:03:40 +0900

    feat: use 処理の実装

--- app/Services/UserStatusService.php
+++ app/Services/UserStatusService.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\UserStatus;
+use App\Repositories\UserStatus\UserStatusRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class UserStatusService
 {
+    use ResponseTrait;
+    use ExceptionHandlerTrait;
+
 }
