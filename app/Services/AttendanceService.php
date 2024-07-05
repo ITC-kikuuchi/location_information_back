3d422a4325e0b072027f2b61722204da62dc0447
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-05 16:30:19 +0900

    feat: use 処理の実装

--- app/Services/AttendanceService.php
+++ app/Services/AttendanceService.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Attendance;
+use App\Repositories\Attendance\AttendanceRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class AttendanceService
 {
+    use ResponseTrait;
+    use ExceptionHandlerTrait;
+
 }
