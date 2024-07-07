2a91e90a89dd7576677c37d7ffba3713b4dd8376
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-07 22:08:50 +0900

    feat: use 処理の実装

--- app/Services/UserLocationService.php
+++ app/Services/UserLocationService.php
@@ -4,6 +4,18 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Area;
+use App\Models\Attendance;
+use App\Models\User;
+use App\Models\UserStatus;
+use App\Repositories\User\UserRepositoryInterface;
+use App\Traits\ExceptionHandlerTrait;
+use App\Traits\ResponseTrait;
+use Exception;
+use Illuminate\Http\JsonResponse;
+
 class UserLocationService
 {
+    use ResponseTrait;
+    use ExceptionHandlerTrait;
 }
