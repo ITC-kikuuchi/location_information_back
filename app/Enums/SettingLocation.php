98a61c1be7b9ac210f6d082b61634c5548e857fe
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-10 17:45:34 +0900

    feat: 勤怠ID に紐づくエリアID を返却する処理の実装

--- app/Enums/SettingLocation.php
+++ app/Enums/SettingLocation.php
@@ -9,4 +9,22 @@ enum SettingLocation: int
     case THREE = 3;
     case FOUR = 4;
     case FIVE = 5;
+
+    /**
+     * 勤怠IDに紐づくエリアIDの返却
+     *
+     * @param integer $attendance_id
+     * @param integer $default_area_id
+     * @return integer
+     */
+    public static function getAreaId(int $attendance_id, int $default_area_id): int
+    {
+        return match ($attendance_id) {
+            self::ONE->value => $default_area_id, // デフォルトエリア
+            self::TWO->value => 5, // 自宅
+            self::THREE->value => 6, // なし
+            self::FOUR->value => 6, // なし
+            self::FIVE->value => 6, // なし
+        };
+    }
 }
