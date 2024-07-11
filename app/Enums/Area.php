2d651e941b7219292bfc4f254665b648cafbab12
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-11 13:55:41 +0900

    feat: 定数の実装

--- app/Enums/Area.php
+++ app/Enums/Area.php
@@ -4,4 +4,16 @@ namespace App\Enums;
 
 enum Area: int
 {
+    // 2F開発部屋
+    case SECOND_FLOOR_DEVELOPMENT_ROOM = 1;
+    // 2F会議室
+    case SECOND_FLOOR_CONFERENCE_ROOM = 2;
+    // 1F開発部屋
+    case FIRST_FLOOR_DEVELOPMENT_ROOM = 3;
+    // 1F会議室
+    case FIRST_FLOOR_CONFERENCE_ROOM = 4;
+    // 自宅
+    case HOME = 5;
+    // なし
+    case NONE = 6;
 }
--- app/Enums/UserStatus.php
+++ app/Enums/UserStatus.php
@@ -4,4 +4,14 @@ namespace App\Enums;
 
 enum UserStatus: int
 {
+    // 自席
+    case OWN_SEAT = 1;
+    // 離席
+    case LEAVE_SEAT = 2;
+    // 会議中
+    case IN_A_MEETING = 3;
+    // 外出中
+    case OUTING = 4;
+    // なし
+    case NONE = 5;
 }
