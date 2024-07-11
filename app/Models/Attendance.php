46df377c51716f3f8995c89b43058632ffc395f1
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-11 09:55:45 +0900

    feat: 勤怠の登録値の定数の実装

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -29,4 +29,16 @@ class Attendance extends Model
 
     // レスポンスで使用する key
     const ATTENDANCE_LIST = 'attendance_list';
+    // 出社
+    const GOING_TO_WORK = 1;
+    // リモート
+    const REMOTE = 2;
+    // 退勤
+    const CLOCKING_OUT = 3;
+    // 出張
+    const BUSINESS_TRIP = 4;
+    // 休暇
+    const VACATION = 5;
+    // なし
+    const NONE = 6;
 }
