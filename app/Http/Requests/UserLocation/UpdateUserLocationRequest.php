104fb73cad48fabb96431ccd77cfaf896c2ee206
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-11 14:19:30 +0900

    feat: use 処理の実装

--- app/Http/Requests/UserLocation/UpdateUserLocationRequest.php
+++ app/Http/Requests/UserLocation/UpdateUserLocationRequest.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Requests\UserLocation;
 
+use App\Enums\Area;
+use App\Enums\Attendance;
+use App\Enums\UserStatus;
+use App\Models\User;
+use App\Traits\ResponseTrait;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class UpdateUserLocationRequest extends FormRequest
 {
+    use ResponseTrait;
+
     /**
      * Determine if the user is authorized to make this request.
      */
