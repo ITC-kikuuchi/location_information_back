a261bebd55adf5d6a62446f7e8aac4bb9b9af9ed
kikuuchi <kikuuchi@itcowork.co.jp>
2024-07-16 14:55:18 +0900

    fix: 不要な処理の削除

--- app/Http/Controllers/UserStatusController.php
+++ app/Http/Controllers/UserStatusController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Services\UserStatusService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class UserStatusController extends Controller
 {
